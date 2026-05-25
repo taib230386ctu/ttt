6a57c7046684e312de9e5e84370fdd1a523d53e4
taib2303846CTU <taib2303846@student.ctu.edu.vn>
2026-05-26 00:10:24 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('btnYes').onclick = () => {
+            window.location.href = "index.html"; // Chuyển vào Galaxy
+        };
+        document.getElementById('btnNo').onclick = () => {
+            alert("thong bao tam");
+            window.location.href = "index.html";
+        };
